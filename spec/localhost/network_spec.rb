a3ece70a9c7c57fa39820d6068b2b4679588b931
Paul Gomersbach <p.gomersbach@rely.nl>
2015-10-14 21:24:17 +0200

    more info on checks

--- spec/localhost/network_spec.rb
+++ spec/localhost/network_spec.rb
@@ -1,26 +1,29 @@
 require 'spec_helper'
 
-describe 'basic network checks' do
+describe 'Basic network checks' do
   gateway = `ip route show | awk '$3 ~ /^[1-9]+/ {print $3;}'`.strip
-
-  describe routing_table do
-    it { should have_entry( :destination => 'default') }
+  describe 'Check default gateway' do
+    describe routing_table do
+      it { should have_entry( :destination => 'default', :gateway => "#{gateway}") }
+    end
+    describe host("#{gateway}") do
+      it { should be_reachable }
+    end
   end
-
-  describe host("#{gateway}") do
-    it { should be_reachable }
+  describe 'Check name resolving' do
+    describe host('google.com') do
+      it { should be_resolvable.by('dns') }
+    end
   end
-
-  describe host('google.com') do
-    it { should be_resolvable.by('dns') }
+  describe 'Check internal connectivity' do
+    describe host('kilo.naturalis.nl') do
+      it { should be_resolvable }
+      it { should be_reachable }
+    end
   end
-
-  describe host('kilo.naturalis.nl') do
-    it { should be_resolvable }
-    it { should be_reachable.with( :port => 443, :proto => 'tcp' ) }
-  end
-
-  describe host('8.8.8.8') do
-    it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
+  describe 'Check external connectivity' do
+    describe host('8.8.8.8') do
+      it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
+    end
   end
 end
