4a080e3e74c34f22708ad257b51ba272438c0769
Paul Gomersbach <p.gomersbach@rely.nl>
2015-10-21 12:50:36 +0200

    test port 443 on kilo.naturalis.nl get nat ip address

--- spec/localhost/network_spec.rb
+++ spec/localhost/network_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
+require 'net/http'
 
 describe 'Basic network checks' do
-  gateway = `ip route show | awk '$3 ~ /^[1-9]+/ {print $3;}'`.strip
+  gateway = `ip route show | awk '$3 ~ /^[1-9]+/ {print $3;}'|head -1`.strip
+  naturl = 'myexternalip.com'
+  natip = Net::HTTP.get("#{naturl}", '/raw')
+
   describe 'Check default gateway' do
     describe routing_table do
       it { should have_entry( :destination => 'default', :gateway => "#{gateway}") }
@@ -18,12 +22,16 @@ describe 'Basic network checks' do
   describe 'Check internal connectivity' do
     describe host('kilo.naturalis.nl') do
       it { should be_resolvable }
-      it { should be_reachable }
+      it { should be_reachable.with( :port => 443, :proto => 'tcp' ) }
     end
   end
   describe 'Check external connectivity' do
     describe host('8.8.8.8') do
       it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
+    end
+  end
+   describe 'Check outside NAT address' do
+    it "#{natip} is the outside, translated ip address" do
     end
   end
 end
