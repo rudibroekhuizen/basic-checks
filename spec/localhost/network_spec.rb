473088dd932d9b551494dc48035a697877c80ba1
Paul Gomersbach <p.gomersbach@rely.nl>
2015-11-10 21:34:47 +0100

    add ejournals check

--- spec/localhost/network_spec.rb
+++ spec/localhost/network_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 require 'net/ip'
 require 'net/http'
+require 'yaml'
+
+public def working_url?(url_str)
+  url = URI.parse(url_str)
+  Net::HTTP.start(url.host, url.port) do |http|
+    http.head(url.request_uri).code == '200'
+  end
+#  true
+rescue
+  false
+end
+
 
 describe 'Basic network checks' do
   naturl = 'myexternalip.com'
   natip = Net::HTTP.get("#{naturl}", '/raw')
   mygateway = Net::IP.routes.gateways[0].via
+
+  ejournals = YAML.load_file('ejournals.yaml')
+#  puts ejournals['ejournalurls']
 
   describe 'Check default gateway' do
     describe routing_table do
@@ -31,8 +46,19 @@ describe 'Basic network checks' do
       it { should be_reachable.with( :port => 53, :proto => 'udp' ) }
     end
   end
-   describe 'Check outside NAT address' do
+  describe 'Check outside NAT address' do
     it "#{natip} is the outside, translated ip address" do
     end
+  end
+
+  describe 'Check e-journals' do
+     ejournals['ejournalurls'].each do |url|
+       describe url do
+         it { should be_working_url(url) }
+       end 
+#       it "#{url} can be downloaded" do
+#         working_url(url)
+#       end
+     end
   end
 end
